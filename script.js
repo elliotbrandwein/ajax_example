49cc0529a5b587f204200bde27a54ffb6d6904f1
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-09 12:05:09 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function search(){
+
+var test  = $.ajax(
+{
+   url: "http://www.omdbapi.com/?",   data: { t: $('input[name=t]').val() }
+   ,   dataType: "json",   success: function(response)
+  {   console.log(response);
+    if(response.Response=="False")
+    {
+      $("#return").html('<h1>'+" no movie with that name found"+'</h1>');
+      $("#plot").html("");
+      $("#poster").html("");
+    }
+    else
+    {
+      $("#return").html('<h1> ' + response.Title + ' </h1>');
+      $("#plot").html(response.Plot);
+      $("#poster").html('<img src = " ' + response.Poster + ' "/>');
+    }
+  } 
+});
+
+return false;
+}
